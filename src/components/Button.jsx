917d924e8a5894c53abae236714b39f2739a6926
Inshal Haq <inshalulhaq11@gmail.com>
2024-03-14 23:54:21 -0500

    Complete SuperQuality section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,14 @@
 const Button = ({ label, iconUrl }) => {
   return (
-    <button className='flex items-center justify-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red'>
+    <button className="flex items-center justify-center gap-2 rounded-full border border-coral-red bg-coral-red px-7 py-4 font-montserrat text-lg leading-none text-white">
       {label}
-      <img
-        src={iconUrl}
-        alt='arrow right icon'
-        className='ml-2 rounded-full w-5 h-5'
-      />
+      {iconUrl && (
+        <img
+          src={iconUrl}
+          alt="arrow right icon"
+          className="ml-2 h-5 w-5 rounded-full"
+        />
+      )}
     </button>
   );
 };
