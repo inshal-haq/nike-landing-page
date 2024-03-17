5fc119b928b6f656dea363976e5fb6f5e4f96f95
Inshal Haq <inshalulhaq11@gmail.com>
2024-03-17 16:08:22 -0500

    Complete Subscribe section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,10 +4,11 @@ const Button = ({
   backgroundColor,
   textColor,
   borderColor,
+  fullWidth,
 }) => {
   return (
     <button
-      className={`flex items-center justify-center gap-2 rounded-full border px-7 py-4 font-montserrat text-lg leading-none
+      className={`flex items-center justify-center gap-2 rounded-full border px-7 py-4 font-montserrat text-lg leading-none ${fullWidth && "w-full"}
       ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` : "border-coral-red bg-coral-red text-white"}
     `}
     >
