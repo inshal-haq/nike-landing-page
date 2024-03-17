69156db86420bbc43bba143223b88bc402a6754c
Inshal Haq <inshalulhaq11@gmail.com>
2024-03-17 15:59:40 -0500

    Complete CustomerReviews section

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,5 +1,31 @@
+import { reviews } from "../constants";
+import ReviewCard from "../components/ReviewCard";
+
 const CustomerReviews = () => {
-  return <div>CustomerReviews</div>;
+  return (
+    <section className="max-container">
+      <h3 className="text-center font-palanquin text-4xl font-bold">
+        What Our
+        <span className="text-coral-red"> Customers </span>
+        Say?
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">
+        Hear genuine stories from our satisfied customers about their
+        exceptional experiences with us.
+      </p>
+      <div className="mt-24 flex flex-1 items-center justify-evenly gap-14 max-lg:flex-col">
+        {reviews.map((item) => (
+          <ReviewCard
+            key={item.customerName}
+            imgURL={item.imgURL}
+            customerName={item.customerName}
+            rating={item.rating}
+            feedback={item.feedback}
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default CustomerReviews;
